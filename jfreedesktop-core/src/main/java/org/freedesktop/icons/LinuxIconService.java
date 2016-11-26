3657a237a3e0c21543a873a5279520c6c2e0614e
Brett Smith <brett@nervepoint.com>
2016-11-26 22:43:28 +0000

    Allow icon theme detection to be disable.

--- jfreedesktop-core/src/main/java/org/freedesktop/icons/LinuxIconService.java
+++ jfreedesktop-core/src/main/java/org/freedesktop/icons/LinuxIconService.java
@@ -16,13 +16,11 @@ import java.util.Properties;
  * Tested on Ubuntu 8.04 LTS.
  */
 public class LinuxIconService extends DefaultIconService {
-
 	public LinuxIconService() throws IOException, ParseException {
 		super();
-		checkAndAddBase(new File(System.getProperty("user.home")
-				+ File.separator + ".local"+ File.separator + "share"+ File.separator + "icons"));
-		checkAndAddBase(new File(System.getProperty("user.home")
-				+ File.separator + ".icons"));
+		checkAndAddBase(new File(
+				System.getProperty("user.home") + File.separator + ".local" + File.separator + "share" + File.separator + "icons"));
+		checkAndAddBase(new File(System.getProperty("user.home") + File.separator + ".icons"));
 		checkAndAddBase(new File("/usr/share/icons"));
 	}
 
@@ -31,73 +29,63 @@ public class LinuxIconService extends DefaultIconService {
 		// String selectedThemeName =
 		// getGConfValue("/desktop/gnome/interface/icon_theme", null);
 		String selectedThemeName = null;
-
-		// Cinnamon
-		String cinnamon = System.getenv("CINNAMON_VERSION");
-		if (cinnamon != null) {
-			DConf node = DConf.$.node("org/cinnamon/desktop/interface/");
-			DConf.Entry<String> se = node.getStringEntry("icon-theme");
-			if (se.isExists()) {
-				selectedThemeName = se.getValue();
-			}
-		}
-
-		// GTK3 can store icon theme here
-		if (selectedThemeName == null) {
-			File gtk3 = new File(new File(new File(new File(
-					System.getProperty("user.home")), ".config"), "gtk-3.0"),
-					"settings.ini");
-			if (gtk3.exists()) {
-				try {
-					FileInputStream fin = new FileInputStream(gtk3);
-					try {
-						Properties p = new Properties();
-						p.load(fin);
-						selectedThemeName = p
-								.getProperty("gtk-icon-theme-name");
-					} finally {
-						fin.close();
-					}
-				} catch (IOException ioe) {
+		if (!"true".equals(System.getProperty("jfreedesktop.alwaysUseDefaultTheme", "false"))) {
+			// Cinnamon
+			String cinnamon = System.getenv("CINNAMON_VERSION");
+			if (cinnamon != null) {
+				DConf node = DConf.$.node("org/cinnamon/desktop/interface/");
+				DConf.Entry<String> se = node.getStringEntry("icon-theme");
+				if (se.isExists()) {
+					selectedThemeName = se.getValue();
 				}
 			}
-		}
-
-		if (selectedThemeName == null) {
-			// Use the pretty dumb GConf class
-			GConf node = GConf.$.node("desktop").node("gnome")
-					.node("interface");
-			GConf.Entry<String> en = node.getStringEntry("icon_theme");
-			selectedThemeName = en == null ? null : en.getValue();
-
+			// GTK3 can store icon theme here
 			if (selectedThemeName == null) {
-				// Maybe gconf-tool is installed
-				selectedThemeName = getGConfValue(
-						"/desktop/gnome/interface/icon_theme", null);
-			}
-		}
-
-		// Load the theme, or fallback to a default that may be installed
-		if (selectedThemeName != null) {
-			theme = getEntity(selectedThemeName);
-			if (theme == null) {
-
-				if (defaultThemeName != null) {
-					theme = getEntity(defaultThemeName);
-					if (theme != null) {
-						return theme;
-					}
-				}
-
-				for (String name : new String[] { "Tango", "Human", "Humanity" }) {
-					theme = getEntity(name);
-					if (theme != null) {
-						break;
+				File gtk3 = new File(new File(new File(new File(System.getProperty("user.home")), ".config"), "gtk-3.0"),
+						"settings.ini");
+				if (gtk3.exists()) {
+					try {
+						FileInputStream fin = new FileInputStream(gtk3);
+						try {
+							Properties p = new Properties();
+							p.load(fin);
+							selectedThemeName = p.getProperty("gtk-icon-theme-name");
+						} finally {
+							fin.close();
+						}
+					} catch (IOException ioe) {
 					}
 				}
 			}
+			if (selectedThemeName == null) {
+				// Use the pretty dumb GConf class
+				GConf node = GConf.$.node("desktop").node("gnome").node("interface");
+				GConf.Entry<String> en = node.getStringEntry("icon_theme");
+				selectedThemeName = en == null ? null : en.getValue();
+				if (selectedThemeName == null) {
+					// Maybe gconf-tool is installed
+					selectedThemeName = getGConfValue("/desktop/gnome/interface/icon_theme", null);
+				}
+			}
+			// Load the theme, or fallback to a default that may be installed
+			if (selectedThemeName != null) {
+				theme = getEntity(selectedThemeName);
+				if (theme == null) {
+					if (defaultThemeName != null) {
+						theme = getEntity(defaultThemeName);
+						if (theme != null) {
+							return theme;
+						}
+					}
+					for (String name : new String[] { "Tango", "Human", "Humanity" }) {
+						theme = getEntity(name);
+						if (theme != null) {
+							break;
+						}
+					}
+				}
+			}
 		}
-
 		// Finally use the default supplied tango theme
 		if (theme == null) {
 			theme = super.getDefaultTheme();
@@ -109,8 +97,7 @@ public class LinuxIconService extends DefaultIconService {
 		ProcessBuilder bui = new ProcessBuilder("gconftool", "--get", key);
 		try {
 			Process p = bui.start();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					p.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			try {
 				String val = reader.readLine();
 				p.waitFor();
@@ -121,9 +108,7 @@ public class LinuxIconService extends DefaultIconService {
 				reader.close();
 			}
 		} catch (IOException ioe) {
-			System.out
-					.println("WARNING: Failed to get gconf value. Using default of "
-							+ defaultValue);
+			System.out.println("WARNING: Failed to get gconf value. Using default of " + defaultValue);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -131,6 +116,7 @@ public class LinuxIconService extends DefaultIconService {
 	}
 
 	public static void main(String[] args) throws Exception {
-		new LinuxIconService().postInit();;
+		new LinuxIconService().postInit();
+		;
 	}
 }
